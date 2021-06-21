c47f17d75b8393881aa76d2aac10d9b0888ac989
Philip Mavlyutov <Misterio715>
2021-06-21 14:17:33 +0300

    WPF examples add

---
+++ WPF examples/Comp_Graph_1/Comp_Graph_1/MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Comp_Graph_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PlotArea Graph1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Graph1 = new PlotArea(MyChart);
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyChart.Cursor = System.Windows.Forms.Cursors.Hand;
+            Graph1.Plot(MyTextBox.Text);
+        }
+        private class ZoomFrame
+        {
+            public double XStart { get; set; }
+            public double XFinish { get; set; }
+            public double YStart { get; set; }
+            public double YFinish { get; set; }
+        }
+        private Stack<ZoomFrame> _zoomFrames = new Stack<ZoomFrame>();
+        private void MyChart_MouseWheel(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            try
+            {
+                if (e.Delta < 0)
+                {
+                    if (_zoomFrames.Count > 0)
+                    {
+                        ZoomFrame frame = _zoomFrames.Pop();
+                        if (_zoomFrames.Count == 0)
+                        {
+                            MyChart.ChartAreas[0].AxisX.ScaleView.ZoomReset();
+                            MyChart.ChartAreas[0].AxisY.ScaleView.ZoomReset();
+                        }
+                        else
+                        {
+                            MyChart.ChartAreas[0].AxisX.ScaleView.Zoom(frame.XStart, frame.XFinish);
+                            MyChart.ChartAreas[0].AxisY.ScaleView.Zoom(frame.YStart, frame.YFinish);
+                        }
+                    }
+                }
+                else if (e.Delta > 0)
+                {
+                    var xMin = MyChart.ChartAreas[0].AxisX.ScaleView.ViewMinimum;
+                    var xMax = MyChart.ChartAreas[0].AxisX.ScaleView.ViewMaximum;
+                    var yMin = MyChart.ChartAreas[0].AxisY.ScaleView.ViewMinimum;
+                    var yMax = MyChart.ChartAreas[0].AxisY.ScaleView.ViewMaximum;
+
+                    _zoomFrames.Push(new ZoomFrame { XStart = xMin, XFinish = xMax, YStart = yMin, YFinish = yMax });
+
+                    var posXStart = MyChart.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.X) - (xMax - xMin) / 4;
+                    var posXFinish = MyChart.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.X) + (xMax - xMin) / 4;
+                    var posYStart = MyChart.ChartAreas[0].AxisY.PixelPositionToValue(e.Location.Y) - (yMax - yMin) / 4;
+                    var posYFinish = MyChart.ChartAreas[0].AxisY.PixelPositionToValue(e.Location.Y) + (yMax - yMin) / 4;
+
+                    MyChart.ChartAreas[0].AxisX.ScaleView.Zoom(posXStart, posXFinish);
+                    MyChart.ChartAreas[0].AxisY.ScaleView.Zoom(posYStart, posYFinish);
+                }
+            }
+            catch { }
+        }
+        private double PrevX;
+        private double PrevY;
+        private void MyChart_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            try
+            {
+                if (!e.Button.HasFlag(System.Windows.Forms.MouseButtons.Left)) return;
+                Axis ax = MyChart.ChartAreas[0].AxisX;
+                Axis ay = MyChart.ChartAreas[0].AxisY;
+                double RangeX = ax.Maximum - ax.Minimum;
+                double RangeY = ay.Maximum - ay.Minimum;
+                double NewX = ax.PixelPositionToValue(e.Location.X);
+                double NewY = ay.PixelPositionToValue(e.Location.Y);
+                ax.Minimum -= (NewX - PrevX);
+                ax.Maximum = ax.Minimum + RangeX;
+                ay.Minimum -= (NewY - PrevY);
+                ay.Maximum = ay.Minimum + RangeY;
+            }
+            catch { }
+        }
+        private void MyChart_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            Axis ax = MyChart.ChartAreas[0].AxisX;
+            Axis ay = MyChart.ChartAreas[0].AxisY;
+            PrevX = ax.PixelPositionToValue(e.Location.X);
+            PrevY = ay.PixelPositionToValue(e.Location.Y);
+        }
+    }
+    class PlotArea
+    {
+        private Chart FuncGraph { get; set; }
+        private double a { get; set; } = 1;
+        public PlotArea(Chart MyChart)
+        {
+            FuncGraph = MyChart;
+        }
+        public void Plot(string arg)
+        {
+            try
+            {
+                a = Convert.ToDouble(arg);
+            }
+            catch { }
+            FuncGraph.ChartAreas.Clear();
+            //FuncGraph.Series.Clear();
+            FuncGraph.ChartAreas.Add(new ChartArea());
+            Series FuncGraphPoints = new Series();
+            FuncGraphPoints.ChartType = SeriesChartType.Line;
+            FuncGraphPoints.Color = System.Drawing.Color.Blue;
+            double r;
+            for (double phi = -Math.PI; phi <= Math.PI; phi += 0.001)
+            {
+                r = a * Math.Cos(3 * phi);
+                FuncGraphPoints.Points.AddXY(r * Math.Cos(phi), r * Math.Sin(phi));
+            }
+            SetSize();
+            FuncGraph.Series.Add(FuncGraphPoints);
+        }
+        private void SetSize()
+        {
+            FuncGraph.ChartAreas[0].AxisX.Minimum = -a * 1.6;
+            FuncGraph.ChartAreas[0].AxisX.Maximum = a * 1.6;
+            FuncGraph.ChartAreas[0].AxisY.Minimum = -a * 0.9;
+            FuncGraph.ChartAreas[0].AxisY.Maximum = a * 0.9;
+        }
+    }
+}
---
+++ WPF examples/Comp_Graph_2/Comp_Graph_2/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Media3D;
+
+namespace Comp_Graph_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Pyramid MyPyramid { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MyPyramid = new Pyramid(MyViewport);
+            }
+            catch { }
+        }
+
+        private void SliderY_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            MyPyramid.RotationY((Slider)sender);
+        }
+
+        private void SliderX_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            MyPyramid.RotationX((Slider)sender);
+        }
+    }
+    class Pyramid
+    {
+        private Viewport3D MyViewport { get; set; }
+        private ModelVisual3D Figure { get; set; } = new ModelVisual3D();
+        private ModelVisual3D Light { get; set; } = new ModelVisual3D();
+        private AxisAngleRotation3D rotateY = new AxisAngleRotation3D();
+        private AxisAngleRotation3D rotateX = new AxisAngleRotation3D();
+
+        private MatrixTransform3D rotatey = new MatrixTransform3D();
+        private MatrixTransform3D rotatex = new MatrixTransform3D();
+        private Matrix3D rotate = new Matrix3D();
+
+        private RotateTransform3D MyRotateY { get; set; }
+        private RotateTransform3D MyRotateX { get; set; }
+        private double x { get; set; } = 5;
+        private double centerZ { get; set; }
+        private double angleY { get; set; } = 0;
+        private double angleX { get; set; } = 0;
+        public Pyramid(Viewport3D obj)
+        {
+            MyViewport = obj;
+            centerZ = -x * Math.Sqrt(2) / Math.Sqrt(5 - Math.Sqrt(5));
+            //rotatey.Matrix = rotate;
+            SetCamera();
+            SetLight();
+            DrawFigure();
+            MyRotateY = new RotateTransform3D(rotateY, 0, 0, 0);
+            MyRotateX = new RotateTransform3D(rotateX, 0, 0, 0);
+            Transform3DGroup MyTransform = new Transform3DGroup();
+            MyTransform.Children.Add(MyRotateY);
+            MyTransform.Children.Add(MyRotateX);
+            //MyTransform.Children.Add(rotatey);
+            //MyTransform.Children.Add(rotatex);
+            Figure.Transform = MyTransform;
+        }
+        private void SetCamera()
+        {
+            MyViewport.Camera = new PerspectiveCamera() { Position = new Point3D(0, 5, 30), LookDirection = new Vector3D(0, 0, -10) };
+        }
+        private void SetLight()
+        {
+            Light.Content = new DirectionalLight() { Color = Colors.White, Direction = new Vector3D(0, 1, -1) };
+            MyViewport.Children.Add(Light);
+        }
+        public void RotationY(Slider SliderY)
+        {
+            //angleY = SliderY.Value;
+            //rotatey.Matrix = new Matrix3D(Math.Cos(angleY * Math.PI / 180), 0, -Math.Sin(angleY * Math.PI / 180), 0, 0, 1, 0, 0, Math.Sin(angleY * Math.PI / 180), 0, Math.Cos(angleY * Math.PI / 180), 0, 0, 0, 0, 1);
+            rotateY.Axis = new Vector3D(0, Math.Cos(angleX * Math.PI / 180), -Math.Sin(angleX * Math.PI / 180));
+            rotateY.Angle = SliderY.Value;
+            angleY = SliderY.Value;
+        }
+        public void RotationX(Slider SliderX)
+        {
+            //angleX = SliderX.Value;
+            //rotatex.Matrix = new Matrix3D(1, 0, 0, 0, 0, Math.Cos(angleX * Math.PI / 180), Math.Sin(angleX * Math.PI / 180), 0, 0, -Math.Sin(angleX * Math.PI / 180), Math.Cos(angleX * Math.PI / 180), 0, 0, 0, 0, 1);
+            rotateX.Axis = new Vector3D(Math.Cos(angleY * Math.PI / 180), 0, 0);
+            rotateX.Angle = SliderX.Value;
+            angleX = SliderX.Value;
+        }
+        public void DrawFigure()
+        {
+            GeometryModel3D Pyramid = new GeometryModel3D();
+            Point3DCollection Points = new Point3DCollection(6);
+            Points.Add(new Point3D(0, 0, -centerZ));
+            Points.Add(new Point3D(-x * Math.Cos(Math.PI / 5), 0, -x * Math.Sin(Math.PI / 5) - centerZ));
+            Points.Add(new Point3D(x * Math.Sin(Math.PI / 10) - x * Math.Cos(Math.PI / 5), 0, -x * Math.Sin(Math.PI / 5) - x * Math.Cos(Math.PI / 10) - centerZ));
+            Points.Add(new Point3D(x * Math.Cos(Math.PI / 5), 0, -x * Math.Sin(Math.PI / 5) - centerZ));
+            Points.Add(new Point3D(-x * Math.Sin(Math.PI / 10) + x * Math.Cos(Math.PI / 5), 0, -x * Math.Sin(Math.PI / 5) - x * Math.Cos(Math.PI / 10) - centerZ));
+            Points.Add(new Point3D(0, 10, 0));
+            Pyramid.Geometry = new MeshGeometry3D() { Positions = Points, TriangleIndices = new Int32Collection() { 4, 3, 0, 0, 1, 2, 2, 4, 0, 0, 5, 1, 1, 5, 2, 2, 5, 4, 4, 5, 3, 3, 5, 0 } };
+            Pyramid.Material = new DiffuseMaterial() { Brush =  Brushes.Blue };
+            Figure.Content = Pyramid;
+            MyViewport.Children.Add(Figure);
+        }
+    }
+}
